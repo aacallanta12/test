c683e5529c030d3dc0539d0b2bcd1648dd7abcf9
Ariel Callanta <arielcallanta@gmail.com>
2015-01-31 12:53:52 +0800

    modified controllers in main.js

--- mytodo/app/scripts/controllers/main.js
+++ mytodo/app/scripts/controllers/main.js
@@ -7,11 +7,9 @@
  * # MainCtrl
  * Controller of the todoApp
  */
-angular.module('todoApp')
+'use strict';
+
+angular.module('mytodoApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.todos = ['Item 1', 'Item 2', 'Item 3'];
   });
